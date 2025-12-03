35065c7b78bf1217ede13c5af099b196beeabc93
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-12-03 14:59:05 -0300

    Added elixir and ruby dev-tools

--- modules/dev-tools.nix
+++ modules/dev-tools.nix
@@ -3,22 +3,24 @@
 {
   environment.systemPackages = with pkgs; [
     rustup
-		rust-analyzer
-    nodejs_22
-    nodePackages.pnpm
-    nodePackages.typescript-language-server
-    gnumake
-    docker
-    docker-compose
-    beekeeper-studio
-    usql
-    gcc
-    jq
-    ripgrep
-    fzf
-    mongodb-compass
-    bat
-		luarocks
-		ruby-lsp
-  ];
+      rust-analyzer
+      nodejs_22
+      nodePackages.pnpm
+      nodePackages.typescript-language-server
+      gnumake
+      docker
+      docker-compose
+      beekeeper-studio
+      usql
+      gcc
+      jq
+      ripgrep
+      fzf
+      mongodb-compass
+      bat
+      luarocks
+      ruby
+      ruby-lsp
+      elixir
+      ];
 }
